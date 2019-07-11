583d1eaca6a98c52774b24981761840c2311a86a
kurt1288 <3249028+kurt1288@users.noreply.github.com>
2019-07-11 11:42:10 -0700

    Remove htst from startus as it's implemented in proxy

--- Startup.cs
+++ Startup.cs
@@ -38,8 +38,6 @@ namespace MapToGlobe
          else
          {
             app.UseStatusCodePagesWithReExecute("/Home/Error", "?statusCode={0}");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
          }
 
          app.UseForwardedHeaders(new ForwardedHeadersOptions

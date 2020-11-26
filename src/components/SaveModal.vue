1a2a6826e125da636e8f225d8f4ec81791f2ba2b
kurt1288 <3249028+kurt1288@users.noreply.github.com>
2020-11-25 18:58:07 -0800

    add icons to save/error modal

--- src/components/SaveModal.vue
+++ src/components/SaveModal.vue
@@ -31,8 +31,14 @@
             <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div class="sm:flex sm:items-start">
                     <div class="mt-3 text-center sm:mt-0 sm:text-left w-full">
-                        <h3 class="text-2xl leading-6 font-medium text-gray-900 mb-5" id="modal-headline">
-                        {{ Result }}
+                        <h3 class="text-2xl leading-6 font-medium text-gray-900 mb-5 flex" id="modal-headline">
+                            <svg v-if="!success" class="w-7 h-7 text-red-700 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+                            </svg>
+                            <svg v-else class="w-7 h-7 text-green-700 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                            </svg>
+                            <span>{{ Result }}</span>
                         </h3>
                         <div class="mt-2">
                             <div v-if="success">
@@ -50,8 +56,7 @@
                                 <p v-if="id" class="mt-4">You will not see this again. Make sure you copy both URLS!</p>
                             </div>
                             <div v-else>
-                                <p class="leading-5 text-gray-500">There was an error:</p>
-                                <p class="text-sm italic leading-5 text-gray-500 m-2 ml-0 p-3">{{ message }}</p>
+                                <p class="leading-5 text-gray-500 m-2 ml-0 p-3">{{ message }}</p>
                             </div>
                         </div>
                     </div>
@@ -96,7 +101,7 @@
             const privateCopied = ref(false);
 
             const Result = computed(() => {
-                return props.success ? "Save" : "Error";
+                return props.success ? "Success" : "Error";
             });
 
             const shareUrl = computed(() => {

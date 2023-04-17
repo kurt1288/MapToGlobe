39bf219a405d177af3f31a628627b549e9ca2d83
Kurt <3249028+kurt1288@users.noreply.github.com>
2023-04-17 09:11:10 -0700

    Update Contact.vue - remove contact form

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -10,21 +10,13 @@
             <h2 class="text-3xl">Issues?</h2>
             <p>If you have a problem, please first look through <a class="text-blue-600" href="https://github.com/kurt1288/MapToGlobe/issues">the open issues</a>. If the issue you found has not already been reported, you may <a class="text-blue-600" href="https://github.com/kurt1288/MapToGlobe/issues/new">create a new issue</a>.</p>
         </div>
-        <div class="mb-10">
-            <h2 class="text-3xl">Other</h2>
-            <p>Please fill out the form to send me a message. Please note that because it's just me, I may not be able to respond to all message.</p>
-        </div>
-        <div>
-            <ContactForm />
-        </div>
     </div>
 </template>
 
 <script lang="ts">
   import HelpNav from '@/components/HelpNav.vue';
-  import ContactForm from '@/components/ContactForm.vue';
 
   export default ({
-    components: { HelpNav, ContactForm }
+    components: { HelpNav }
   })
 </script>
